bb4c44c730881ec9440519af1a7a5b42c641fb7d
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-15 04:55:21 -0800

    Implement basic shopping cart functionality for three cards

--- src/Shop.js
+++ src/Shop.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import NavBar from "./NavBar";
 import Card from "./shop-components/Card"
 import "./Styles/Shop.css"
@@ -6,17 +7,28 @@ const IncunabulaLink = "https://i.discogs.com/0KFGgi-D9w_TvEsd4q-P8G5Vnexb3rDxSU
 const AmberLink = "https://i.discogs.com/N9vUT9NvRU-5h8-Qf1dGfMkKNFd2Zoilo-Gmt3PWPOs/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI0OTQt/MTE1OTgxNDIzNS5q/cGVn.jpeg";
 const TriRepetaeLink = "https://i.discogs.com/U5ZWcUltD6rFjocqtOO1rQZ60lTq22m4ntZvhEKBxxU/rs:fit/g:sm/q:90/h:599/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTk1OTcx/ODItMTQ4MzM5NjY0/OS00MDMyLmpwZWc.jpeg";
 
-const App = () => {
+const Shop = () => {
+  const [numInCart, setNumInCart] = useState(0);
+
+  const incrementCart = () => {
+    setNumInCart(numInCart + 1);
+  }
+
   return (
     <div>
       <NavBar />
+      <div>Items In Cart: {numInCart}</div>
+      <button>Go To Cart</button>
       <div className="card-container">
-        <Card image={IncunabulaLink} title={"Incunabula"} />
-        <Card image={AmberLink} title={"Amber"} />
-        <Card image={TriRepetaeLink} title={"Tri Repetae"} />
+        <Card image={IncunabulaLink} title={"Incunabula"}
+              incrementCart={incrementCart} />
+        <Card image={AmberLink} title={"Amber"}
+              incrementCart={incrementCart} />
+        <Card image={TriRepetaeLink} title={"Tri Repetae"}
+              incrementCart={incrementCart} />
       </div>
     </div>
   );
 }
 
-export default App;
+export default Shop;
