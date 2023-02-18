cc653a0079507eac846fe3fb0dbf4b752518ac3f
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-17 18:59:05 -0800

    Switch to HashRouter again

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Shop from "./Shop";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL + '/'}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/shop" element={<Shop />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
