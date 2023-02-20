f5f282f7ed111d441aaacaa96476a7d543b817fb
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-19 18:24:35 -0800

    Change navbar image to new logo

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import "./Styles/NavBar.css"
-import logo from "./imgs/VintageAutechreAlbums.png"
+import logo from "./imgs/logo.png"
 
 const NavBar = () => {
   return (
     <div className='navbar'>
-      <img className={logo} src={logo} width={100} />
+      <img className={logo} src={logo} width={50} />
       <Link to={'/'}>Home</Link>
       <Link to={'/shop'}>Shop</Link>
     </div>
