e43c9399c3ec3a65c20309b48644486a24685b52
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-20 07:04:36 -0800

    Remove note text from blurb

--- src/App.js
+++ src/App.js
@@ -8,15 +8,11 @@ const App = () => {
       <NavBar />
       <div className="image-and-blurb">
         <img className="image" src={VAAimg} width={300} />
-        <div className="blurb">Autechre have been true musical pioneers for over thirty
-                               years. Buy digital copies of their first 12 albums here.
-                               If you've never heard Autechre before, strap in - you're
-                               in for an experience like no other!<br></br><br></br>
-                               <em>Note: this is not an actual store. It's simply for
-                               learning purposes. I'm not actually selling anything,
-                               least of all Autechre's music!<br></br>
-                               But you should definitely go listen to Autechre
-                               right now!</em>
+        <div className="blurb">
+          Autechre have been true musical pioneers for over thirty years.
+          Buy digital copies of their first 12 albums here.<br></br><br></br>
+          If you've never heard Autechre before, strap in - you're in for an
+          experience like no other!
         </div>
       </div>
     </div>
