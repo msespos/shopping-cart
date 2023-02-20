0fee4369008b99d04d6920ef4af874062af34f63
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-19 23:12:59 -0800

    Add image and blurb

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import NavBar from "./NavBar";
-
-import logo from "./imgs/VintageAutechreAlbums.png"
+import VAAimg from "./imgs/VintageAutechreAlbums.png"
+import "./Styles/App.css"
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <img src={logo} />
+      <div className="image-and-blurb">
+        <img className="image" src={VAAimg} width={300} />
+        <div className="blurb">Autechre have been true musical pioneers for over thirty
+                               years. Buy digital copies of their first 12 albums here.
+                               If you've never heard Autechre before, strap in - you're
+                               in for an experience like no other!<br></br><br></br>
+                               <em>Note: this is not an actual store. It's simply for
+                               learning purposes. I'm not actually selling anything,
+                               least of all Autechre's music!<br></br>
+                               But you should definitely go listen to Autechre
+                               right now!</em>
+        </div>
+      </div>
     </div>
   );
 }
