6a80ea43dbc836c3c81b538cbe80507753bea09a
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-20 07:05:03 -0800

    Add note to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,10 +4,19 @@ import logo from "./imgs/logo.png"
 
 const NavBar = () => {
   return (
-    <div className='navbar'>
-      <img className={logo} src={logo} width={50} />
-      <Link to={'/'}>Home</Link>
-      <Link to={'/shop'}>Shop</Link>
+    <div>
+      <div className="note">
+        <em>Note: this is not an actual store. It's simply a site created for
+            learning purposes. I'm not actually selling anything, least of all
+            Autechre's music!<br></br>
+            But you should definitely go listen to Autechre right now!
+        </em>
+      </div>
+      <div className='navbar'>
+        <img src={logo} width={50} />
+        <Link to={'/'}>Home</Link>
+        <Link to={'/shop'}>Shop</Link>
+      </div>
     </div>
   );
 }
