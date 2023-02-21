f84db6e1713b197ab23a4210ccf960a50ae0e107
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-21 10:48:15 -0800

    Move navbar tests to NavBar.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,19 +4,6 @@ import App from "./App";
 import '@testing-library/jest-dom';
 
 describe("App component", () => {
-  it("renders Home and Shop in the navbar", () => {
-    const { getByText } = render(<App />, {wrapper: HashRouter});
-    expect(getByText("Home")).toBeInTheDocument();
-    expect(getByText("Shop")).toBeInTheDocument();
-  });
-
-  it("includes some of the blurb in the navbar note", () => {
-    const { asFragment } = render(<App />, {wrapper: HashRouter});
-    expect(asFragment()).toMatchSnapshot(`
-      this is not an actual store.
-    `)
-  });
-
   it("includes some of the blurb in the body", () => {
     const { asFragment } = render(<App />, {wrapper: HashRouter});
     expect(asFragment()).toMatchSnapshot(`
@@ -29,12 +16,5 @@ describe("App component", () => {
     const bodyImage = screen.getByAltText(/Vintage Autechre Albums/i);
     const imageSource = "VintageAutechreAlbums.png";
     expect(bodyImage).toHaveAttribute("src", imageSource);
-  });
-
-  it("has the correct source for the logo image in the navbar", () => {
-    render(<App />, {wrapper: HashRouter});
-    const logoImage = screen.getByAltText(/VAA Logo/i);
-    const imageSource = "logo.png";
-    expect(logoImage).toHaveAttribute("src", imageSource);
   });
 });
