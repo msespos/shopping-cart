f76c863606b9a06d94baa24df3512c0cd1ef91ce
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-23 13:36:18 -0800

    Add first test for Shop.js

--- src/Shop.test.js
+++ src/Shop.test.js
@@ -1,7 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { HashRouter } from "react-router-dom";
 import Shop from "./Shop";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
 describe("Shop component", () => {
   it("has the correct source for the shopping cart logo image", () => {
@@ -9,5 +11,15 @@ describe("Shop component", () => {
     const logoImage = screen.getByAltText(/A shopping cart logo/i);
     const imageSource = "shopping-cart-icon.svg";
     expect(logoImage).toHaveAttribute("src", imageSource);
+  });
+
+  // test slightly adapted from https://www.youtube.com/watch?v=TuxmnyhPdhA
+  it("calls the alert when Go To Cart is clicked", async () => {
+    // below line from https://stackoverflow.com/questions/53611098/how-can-i-mock-the-window-alert-method-in-jest
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    render(<Shop />, {wrapper: HashRouter});
+    const goToCartButton = screen.getByRole("button", { name: "Go To Cart" });
+    await userEvent.click(goToCartButton);
+    expect(alert).toHaveBeenCalledTimes(1);
   });
 });
