e22cc36d358ab3bb9690a06ec3811c29edebad4c
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-24 05:12:04 -0800

    Add test for Shop.js

--- src/Shop.test.js
+++ src/Shop.test.js
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { HashRouter } from "react-router-dom";
 import Shop from "./Shop";
 import "@testing-library/jest-dom";
@@ -21,5 +21,21 @@ describe("Shop component", () => {
     const goToCartButton = screen.getByRole("button", { name: "Go To Cart" });
     await userEvent.click(goToCartButton);
     expect(alert).toHaveBeenCalledTimes(1);
+  });
+
+  // help from Kuromika (fellow Odinite) for the following test:
+  // "It is a more than reasonable question, it is a good test.
+  //  You could test the parent component (the component that contains both components),
+  //  in the test you could use userEvents to simulate the inputs, the click, and then
+  //  get the cart component and expect the number displayed to be some value."
+
+  it("increments the cart by 1 when 1 is input and the Add To Cart button is clicked", async () => {
+    render(<Shop />, {wrapper: HashRouter});
+    const input = screen.getByLabelText("Incunabula");
+    const addToCartButton = screen.getByTestId("Incunabula");
+    fireEvent.change(input, {target: {value: 1}});
+    await userEvent.click(addToCartButton);
+    const numInCart = screen.getByTestId("num-in-cart");
+    expect(numInCart.textContent).toBe("1");
   });
 });
